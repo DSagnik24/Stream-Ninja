a886d3c170e89c7e4bb664199eb1914c1c5c8f5d
Sagnik Dutta <sagnikduttaofficial18@gmail.com>
2024-12-31 00:18:57 +0530

    New updates3

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Toaster />
     <div className="flex flex-col items-center space-y-5 justify-center py-9">
       <h1 className="text-3xl font-bold text-black-900 dark:text-gray-100 ">
         Welcome to Video Streaming Application
